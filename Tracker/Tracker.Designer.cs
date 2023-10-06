dcb72c10435e1c48d9faca731e27169777560d1b
IShowMeat <95954779+spdcode@users.noreply.github.com>
2023-10-06 17:55:57 +0800

    First Release.

---
+++ Tracker/Tracker.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace CovidTracker
+{
+    partial class Tracker
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            DataGridViewCellStyle dataGridViewCellStyle1 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle2 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle3 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle4 = new DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Tracker));
+            resultButton = new Button();
+            DataGrid = new DataGridView();
+            cryptoComboBox = new ComboBox();
+            cryptoName = new DataGridViewTextBoxColumn();
+            cryptoSymbol = new DataGridViewTextBoxColumn();
+            currPrice = new DataGridViewTextBoxColumn();
+            volume = new DataGridViewTextBoxColumn();
+            marketcap = new DataGridViewTextBoxColumn();
+            currSupply = new DataGridViewTextBoxColumn();
+            maxSupply = new DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)DataGrid).BeginInit();
+            SuspendLayout();
+            // 
+            // resultButton
+            // 
+            resultButton.AutoSize = true;
+            resultButton.BackColor = Color.FromArgb(23, 184, 144);
+            resultButton.Font = new Font("Microsoft JhengHei", 16F, FontStyle.Bold, GraphicsUnit.Point);
+            resultButton.ForeColor = Color.White;
+            resultButton.Location = new Point(708, 616);
+            resultButton.Name = "resultButton";
+            resultButton.Size = new Size(351, 55);
+            resultButton.TabIndex = 0;
+            resultButton.Text = "查詢當前貨幣資訊";
+            resultButton.UseVisualStyleBackColor = false;
+            resultButton.Click += button1_Click;
+            // 
+            // DataGrid
+            // 
+            DataGrid.AllowUserToAddRows = false;
+            dataGridViewCellStyle1.BackColor = Color.White;
+            DataGrid.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            DataGrid.BackgroundColor = SystemColors.Window;
+            DataGrid.CellBorderStyle = DataGridViewCellBorderStyle.SingleVertical;
+            DataGrid.ColumnHeadersBorderStyle = DataGridViewHeaderBorderStyle.Single;
+            dataGridViewCellStyle2.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = SystemColors.Control;
+            dataGridViewCellStyle2.Font = new Font("Microsoft JhengHei", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            dataGridViewCellStyle2.ForeColor = SystemColors.WindowText;
+            dataGridViewCellStyle2.SelectionBackColor = SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = DataGridViewTriState.True;
+            DataGrid.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            DataGrid.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            DataGrid.Columns.AddRange(new DataGridViewColumn[] { cryptoName, cryptoSymbol, currPrice, volume, marketcap, currSupply, maxSupply });
+            dataGridViewCellStyle3.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle3.BackColor = Color.White;
+            dataGridViewCellStyle3.Font = new Font("Segoe UI", 16F, FontStyle.Regular, GraphicsUnit.Point);
+            dataGridViewCellStyle3.ForeColor = SystemColors.ControlText;
+            dataGridViewCellStyle3.SelectionBackColor = SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = DataGridViewTriState.False;
+            DataGrid.DefaultCellStyle = dataGridViewCellStyle3;
+            DataGrid.GridColor = SystemColors.ActiveCaptionText;
+            DataGrid.Location = new Point(12, 12);
+            DataGrid.Name = "DataGrid";
+            dataGridViewCellStyle4.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle4.BackColor = Color.White;
+            dataGridViewCellStyle4.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            dataGridViewCellStyle4.ForeColor = SystemColors.WindowText;
+            dataGridViewCellStyle4.SelectionBackColor = SystemColors.Highlight;
+            dataGridViewCellStyle4.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle4.WrapMode = DataGridViewTriState.True;
+            DataGrid.RowHeadersDefaultCellStyle = dataGridViewCellStyle4;
+            DataGrid.RowHeadersWidth = 62;
+            DataGrid.RowTemplate.Height = 60;
+            DataGrid.Size = new Size(1404, 580);
+            DataGrid.TabIndex = 1;
+            DataGrid.CellContentClick += dataGridView1_CellContentClick;
+            // 
+            // cryptoComboBox
+            // 
+            cryptoComboBox.BackColor = Color.White;
+            cryptoComboBox.Font = new Font("Segoe UI", 16F, FontStyle.Bold, GraphicsUnit.Point);
+            cryptoComboBox.ForeColor = Color.Black;
+            cryptoComboBox.FormattingEnabled = true;
+            cryptoComboBox.Items.AddRange(new object[] { "bitcoin", "ethereum", "tether", "dogecoin", "acid" });
+            cryptoComboBox.Location = new Point(1125, 616);
+            cryptoComboBox.Name = "cryptoComboBox";
+            cryptoComboBox.Size = new Size(291, 53);
+            cryptoComboBox.TabIndex = 2;
+            cryptoComboBox.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
+            // 
+            // cryptoName
+            // 
+            cryptoName.HeaderText = "貨幣名稱";
+            cryptoName.MinimumWidth = 8;
+            cryptoName.Name = "cryptoName";
+            cryptoName.Width = 170;
+            // 
+            // cryptoSymbol
+            // 
+            cryptoSymbol.HeaderText = "貨幣符號";
+            cryptoSymbol.MinimumWidth = 8;
+            cryptoSymbol.Name = "cryptoSymbol";
+            cryptoSymbol.Width = 170;
+            // 
+            // currPrice
+            // 
+            currPrice.HeaderText = "當前價格";
+            currPrice.MinimumWidth = 8;
+            currPrice.Name = "currPrice";
+            currPrice.Width = 191;
+            // 
+            // volume
+            // 
+            volume.HeaderText = "成交量";
+            volume.MinimumWidth = 8;
+            volume.Name = "volume";
+            volume.Width = 192;
+            // 
+            // marketcap
+            // 
+            marketcap.HeaderText = "交易量";
+            marketcap.MinimumWidth = 8;
+            marketcap.Name = "marketcap";
+            marketcap.Width = 230;
+            // 
+            // currSupply
+            // 
+            currSupply.HeaderText = "流通供應量";
+            currSupply.MinimumWidth = 8;
+            currSupply.Name = "currSupply";
+            currSupply.Width = 192;
+            // 
+            // maxSupply
+            // 
+            maxSupply.HeaderText = "最大供應量";
+            maxSupply.MinimumWidth = 8;
+            maxSupply.Name = "maxSupply";
+            maxSupply.Width = 195;
+            // 
+            // Tracker
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            ClientSize = new Size(1428, 694);
+            Controls.Add(cryptoComboBox);
+            Controls.Add(DataGrid);
+            Controls.Add(resultButton);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Tracker";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "加密貨幣查詢 App";
+            ((System.ComponentModel.ISupportInitialize)DataGrid).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button resultButton;
+        private DataGridView DataGrid;
+        private ComboBox cryptoComboBox;
+        private DataGridViewTextBoxColumn cryptoName;
+        private DataGridViewTextBoxColumn cryptoSymbol;
+        private DataGridViewTextBoxColumn currPrice;
+        private DataGridViewTextBoxColumn volume;
+        private DataGridViewTextBoxColumn marketcap;
+        private DataGridViewTextBoxColumn currSupply;
+        private DataGridViewTextBoxColumn maxSupply;
+    }
+}
---
+++ Tracker/Tracker.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Net;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Linq;
+using System.Drawing;
+using static CovidTracker.Tracker;
+
+// It used to be CovidTracker using covid tw gov API, but I found out that the 
+// object name is Chinese, which is kinda complicated for the school project, so I decided to switch
+// to another topic, but it's still based on the same logic.
+
+namespace CovidTracker
+{
+    public partial class Tracker : Form
+    {
+
+        // Variables.
+        private string SelectedCrypto = null;
+
+        // API String, idk how where to put variables so I decided to put it here.
+        private string apiURL = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids={0}&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en";
+
+        // Classes
+
+        // Very cool Crypto class, every string variable inside are based on 
+        // coingecko documentation. @source: https://www.coingecko.com/en/api/documentation.
+
+        // get; set; means you can read / write the following value.
+        // and we will be using get here, so we can loop through the value n more...
+
+        public class Crypto
+        {
+            public string id { get; set; }
+            public string symbol { get; set; }
+            public string current_price { get; set; }
+            public string total_volume { get; set; }
+            public string market_cap { get; set; }
+            public string circulating_supply { get; set; }
+            public string max_supply { get; set; }
+        }
+
+        // Function to get every crypto ID.
+        private async void GetComboBoxInfo()
+        {
+
+            // Getting new client.
+            HttpClient client = new HttpClient();
+
+            // We will be using async here to get the Promise result [fulfilled / rejected ]
+            // in short, we use await to wait for the httprequest result.
+            HttpResponseMessage responseBody = await client.GetAsync("https://api.coingecko.com/api/v3/coins/list?include_platform=true");
+
+
+            // 200 i think?
+            if (responseBody.IsSuccessStatusCode)
+            {
+                // awaitable so we have to add await
+                string strResBody = await responseBody.Content.ReadAsStringAsync();
+
+                // Deserialize the JSON response into a list of Coin objects
+                List<Crypto> coins = Newtonsoft.Json.JsonConvert.DeserializeObject<List<Crypto>>(strResBody);
+
+                // Populate the ComboBox with cryptocurrency IDs
+                foreach (Crypto coin in coins)
+                {
+                    cryptoComboBox.Items.Add(coin.id);
+                }
+            }
+            else // Will happen as long as coingecko is dead OR TOO MANT REQUESTS.
+            {
+                MessageBox.Show("An error has occurred, Error Code: " + responseBody.StatusCode);
+            }
+        }
+
+        public Tracker()
+        {
+            InitializeComponent();
+
+            // We will put our comboBox listing function here.
+            GetComboBoxInfo();
+
+            // Example, not necessary but I'll add it to make it look better.
+            DataGrid.Rows.Add(
+            "範例貨幣",
+            "nigga",
+            "69420",
+            "100000",
+            "6942042069",
+            "19000000",
+            "21000000"
+            );
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            // Check if user has already selected an Crypto.
+            if (cryptoComboBox.SelectedItem != null)
+            {
+                SelectedCrypto = cryptoComboBox.SelectedItem.ToString();
+            }
+            else // if bro is too stupid to use this app.
+            {
+                MessageBox.Show("請選擇貨幣!");
+                return;
+            }
+
+
+            // Getting new client.
+            HttpClient client = new HttpClient();
+
+            if (client == null)
+            {
+                MessageBox.Show("HttpClient Error!");
+                return;
+            }
+
+            // Set the header to User-Agent bla bla blah
+            // to pretend that we are a browser and requesting data from the site.
+            client.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36");
+
+            // Pasted from my comboBox since they are the same thing.
+
+            // We will use string.Format(stuff with {0}, text) to make a new URL.
+            string newAPIURL = string.Format(apiURL, cryptoComboBox.SelectedItem.ToString());
+
+            //MessageBox.Show(newAPIURL);
+
+            // We will be using async here to get the Promise result [fulfilled / rejected ]
+            // in short, we use await to wait for the httprequest result.
+            HttpResponseMessage response = await client.GetAsync(newAPIURL);
+
+            // 200 i think?
+            if (response.IsSuccessStatusCode)
+            {
+
+                // awaitable so we have to add await
+                string strResBody = await response.Content.ReadAsStringAsync();
+                List<Crypto> coins = Newtonsoft.Json.JsonConvert.DeserializeObject<List<Crypto>>(strResBody);
+
+                foreach (DataGridViewRow rows in DataGrid.Rows)
+                {
+                    if (rows.Cells["cryptoName"] != null &&
+                        rows.Cells["cryptoName"].Value != null &&
+                        rows.Cells["cryptoName"].Value.ToString() == cryptoComboBox.SelectedItem.ToString()
+                        )
+                    {
+                        MessageBox.Show("您已經選過了!");
+                        return;
+                    }
+                }
+
+                foreach (Crypto coin in coins)
+                {
+                    DataGrid.Rows.Add(
+                        coin.id,
+                        coin.symbol,
+                        coin.current_price,
+                        coin.total_volume,
+                        coin.market_cap,
+                        coin.circulating_supply,
+                        coin.max_supply
+                        );
+                }
+            }
+            else // Will happen as long as coingecko is dead OR TOO MANT REQUESTS.
+            {
+                if (response.StatusCode.ToString() == "TooManyRequests")
+                {
+                    MessageBox.Show("短時間內向API傳輸過多請求\n請稍後再試");
+                    return;
+                }
+                else
+                {
+                    MessageBox.Show("An error has occurred, Error Code: " + response.StatusCode);
+                }
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
